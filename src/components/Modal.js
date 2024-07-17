b52facc3b15ed0de1555b48f55ed35d9654057e0
DESKTOP-GFBT1N7\GunP <gundash1@gmail.com>
2024-07-17 11:54:01 +0700

    add modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,21 +1,21 @@
 export default function Modal(props){
     return <>
   
-        <div class="modal fade" id={props.id} tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel"> {props.title} </h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+        <div className="modal fade" id={props.id} tabIndex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div className="modal-dialog" role="document">
+            <div className="modal-content">
+            <div className="modal-header">
+                <h5 className="modal-title" id="exampleModalLabel"> {props.title} </h5>
+                <button type="button" className="close" data-dismiss="modal" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
                 </button>
             </div>
-            <div class="modal-body">
+            <div className="modal-body">
                 {props.children}
             </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary">Save changes</button>
+            <div className="modal-footer">
+                <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="button" className="btn btn-primary">Save changes</button>
             </div>
             </div>
         </div>
--- src/page/backoffice/Product.js
+++ src/page/backoffice/Product.js
@@ -1,12 +1,16 @@
 import BackOffice from "../../components/BackOffice";
+import Modal from "../../components/Modal";
 
 function Product(){
     return <BackOffice>
         <div className="h4">Product</div>
-        <button className="btn btn-primary">
+        
+        <button className="btn btn-primary" data-toggle="modal" data-target="#modalProduct">
             <i className="fa fa-plus"></i> Add Product
         </button>
+        <Modal id="modalProduct" title="Product"></Modal>
     </BackOffice>
+
 }
 
 export default Product;
