9442020c3d2342f5fb635d8429a84292260dcf8d
DESKTOP-GFBT1N7\GunP <gundash1@gmail.com>
2024-08-13 17:20:24 +0700

    draggable todolist v2

--- src/components/DashboardMain.js
+++ src/components/DashboardMain.js
@@ -1,43 +1,63 @@
 import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
-
+import {useState} from "react"
 
 export default function DashboardMain (){
+
   const startMessage = [
     {
       id: "1",
       subid: "11",
-      title: "todo",
       message: "start", 
     },
     {
       id: "2",
       subid: "12",
-      title: "todo",
+      message: "Middle", 
+    },
+    {
+      id: "3",
+      subid: "13",
       message: "End", 
     }
   ]
+  const [assignment,setAssignment] = useState(startMessage);
 
-  const dragEnd = () =>{
-    
+  const dragEnd = (results) =>{
+    const {destination, source, type} = results
+    if(!destination){ return }
+    if(source.droppableId === destination.droppableId && source.index === destination.index ){ return }
+    if(type){
+      const reOrderedItem = [...assignment];
+      const sourceIndex = source.index
+      const destinationIndex = destination.index
+
+      console.log("sourceIndex",sourceIndex)
+      const [removedItem] =  reOrderedItem.splice(sourceIndex,1)
+      console.log("removedItem",removedItem)
+
+      reOrderedItem.splice(destinationIndex,0,removedItem)
+      console.log("reOrderedItem",reOrderedItem)
+      return setAssignment(reOrderedItem);
+    }
   }
   return <>
-    <DragDropContext onDragEnd={dragEnd()} >
-      {startMessage? startMessage.map((x,indexMain)=>{
+    <DragDropContext onDragEnd={dragEnd} >
+      {assignment? assignment.map((x,index)=>{
         return <>
-          <Droppable droppableId = {x.id} index = {indexMain}>
-            {(provided,indexD1)=>
+          <Droppable droppableId = {x.id}>
+            {(provided)=>
               <div 
                 ref = {provided.innerRef}
                 {...provided.droppableProps}
               >
-                <Draggable draggableId={x.subid} key={x.subid} index={indexD1}>
+                <Draggable draggableId={x.subid} key={x.subid} index={index}>
                 {(provided)=>
-                  <div style={{background:"salmon"}}
+                  <div className="d-flex"
                     {...provided.dragHandleProps}
                     {...provided.draggableProps}
                     ref={provided.innerRef}
                   >
-                      <div > {x.title} </div>
+
                       <div> { x.message}  </div>
                   </div> 
                 }
