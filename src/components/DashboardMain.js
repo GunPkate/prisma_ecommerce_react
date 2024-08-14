d2292ab6cc073a97175a95848ac5aa7ce953c701
DESKTOP-GFBT1N7\GunP <gundash1@gmail.com>
2024-08-14 09:49:43 +0700

    fix dnd board

--- src/components/DashboardMain.js
+++ src/components/DashboardMain.js
@@ -1,102 +1,153 @@
-import { DragDropContext  } from "react-beautiful-dnd";
-import {useState} from "react"
-import DndContainer from "./DndContainer";
+import React, { useState } from "react";
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
 
-export default function DashboardMain (){
+const tasks = [
+  { id: "1", content: "First task" },
+  { id: "2", content: "Second task" },
+  { id: "3", content: "Third task" },
+  { id: "4", content: "Fourth task" },
+  { id: "5", content: "Fifth task" }
+];
 
-  const startMessage = [
-    {
-      id: "1",
-      subid: "11",
-      message: "start", 
-      class: "todo"
-    },
-    {
-      id: "2",
-      subid: "12",
-      message: "Middle", 
-      class: "todo"
-    },
-    {
-      id: "3",
-      subid: "13",
-      message: "End", 
-      class: "todo"
-    },
-    {
-      id: "4",
-      subid: "14",
-      message: "Done", 
-      class: "done"
-    }
-  ]
-  const [assignment,setAssignment] = useState(startMessage);
-
-  const dragEnd = (results) =>{
-    const {destination, source, type} = results
-    if(!destination){ return }
-    if(source.droppableId === destination.droppableId && source.index === destination.index ){ return }
-    
-    if(type){
-      const reOrderedItem = [...assignment];
-      const sourceIndex = source.index
-      const destinationIndex = destination.index
-
-      console.log("sourceIndex",sourceIndex)
-      const [removedItem] =  reOrderedItem.splice(sourceIndex,1)
-      console.log("removedItem",removedItem)
-
-      reOrderedItem.splice(destinationIndex,0,removedItem)
-      console.log("reOrderedItem",reOrderedItem)
-      return setAssignment(reOrderedItem);
-    }
+const taskStatus = {
+  requested: {
+    name: "Requested",
+    items: tasks
+  },
+  toDo: {
+    name: "To do",
+    items: []
+  },
+  inProgress: {
+    name: "In Progress",
+    items: []
+  },
+  done: {
+    name: "Done",
+    items: []
   }
-  return <>
-    <div >
+};
 
-    <DragDropContext onDragEnd={dragEnd} className="bg-gradient-primary">
-        <div className="row">
+const onDragEnd = (result, columns, setColumns) => {
+  if (!result.destination) return;
+  const { source, destination } = result;
 
+  if (source.droppableId !== destination.droppableId) {
+    const sourceColumn = columns[source.droppableId];
+    const destColumn = columns[destination.droppableId];
+    const sourceItems = [...sourceColumn.items];
+    const destItems = [...destColumn.items];
+    const [removed] = sourceItems.splice(source.index, 1);
+    destItems.splice(destination.index, 0, removed);
+    setColumns({
+      ...columns,
+      [source.droppableId]: {
+        ...sourceColumn,
+        items: sourceItems
+      },
+      [destination.droppableId]: {
+        ...destColumn,
+        items: destItems
+      }
+    });
+  } else {
+    const column = columns[source.droppableId];
+    const copiedItems = [...column.items];
+    const [removed] = copiedItems.splice(source.index, 1);
+    copiedItems.splice(destination.index, 0, removed);
+    setColumns({
+      ...columns,
+      [source.droppableId]: {
+        ...column,
+        items: copiedItems
+      }
+    });
+  }
+};
 
-        <div className="bg-gradient-primary col-5 mx-auto my-4 border rounded-lg" style={{ minHeight:"250px"}}>
-          <div className="text-center">To Do</div>
-
-              <div  >
-                <DndContainer dataList={assignment} classD="todo"/>
+function App() {
+  const [columns, setColumns] = useState(taskStatus);
+  return (
+    <div>
+      <h1 style={{ textAlign: "center" }}>Jira Board</h1>
+      <div
+        style={{ display: "flex", justifyContent: "center", height: "100%" }}
+      >
+        <DragDropContext
+          onDragEnd={(result) => onDragEnd(result, columns, setColumns)}
+        >
+          {Object.entries(columns).map(([columnId, column], index) => {
+            return (
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "center"
+                }}
+                key={columnId}
+              >
+                <h2>{column.name}</h2>
+                <div style={{ margin: 8 }}>
+                  <Droppable droppableId={columnId} key={columnId}>
+                    {(provided, snapshot) => {
+                      return (
+                        <div
+                          {...provided.droppableProps}
+                          ref={provided.innerRef}
+                          style={{
+                            background: snapshot.isDraggingOver
+                              ? "lightblue"
+                              : "lightgrey",
+                            padding: 4,
+                            width: 250,
+                            minHeight: 500
+                          }}
+                        >
+                          {column.items.map((item, index) => {
+                            return (
+                              <Draggable
+                                key={item.id}
+                                draggableId={item.id}
+                                index={index}
+                              >
+                                {(provided, snapshot) => {
+                                  return (
+                                    <div
+                                      ref={provided.innerRef}
+                                      {...provided.draggableProps}
+                                      {...provided.dragHandleProps}
+                                      style={{
+                                        userSelect: "none",
+                                        padding: 16,
+                                        margin: "0 0 8px 0",
+                                        minHeight: "50px",
+                                        backgroundColor: snapshot.isDragging
+                                          ? "#263B4A"
+                                          : "#456C86",
+                                        color: "white",
+                                        ...provided.draggableProps.style
+                                      }}
+                                    >
+                                      {item.content}
+                                    </div>
+                                  );
+                                }}
+                              </Draggable>
+                            );
+                          })}
+                          {provided.placeholder}
+                        </div>
+                      );
+                    }}
+                  </Droppable>
+                </div>
               </div>
-
-        </div>
-
-        <div className="bg-gradient-warning col-5 mx-auto my-4 border rounded-lg" style={{  minHeight:"250px"}}>
-          <div className="text-center">In Progress</div>
-
-              <div>
-                <DndContainer dataList={assignment} classD="ip"/>
-              </div>
-
-        </div>
-
-        <div className="bg-gradient-success col-5 mx-auto my-4 border rounded-lg" style={{  minHeight:"250px"}}>
-          <div className="text-center">Done</div>
-
-              <div>
-                <DndContainer dataList={assignment} classD="done"/>
-              </div>
-
-        </div>
-
-        <div className="bg-gradient-info col-5 mx-auto my-4 border rounded-lg" style={{  minHeight:"250px"}}>
-          <div className="text-center">Done</div>
-
-              <div> To Do {assignment.filter(x=>x.class==="todo").length} </div>
-              <div> In Progress {assignment.filter(x=>x.class==="ip").length} </div>
-              <div> Done {assignment.filter(x=>x.class==="done").length} </div>
-
-        </div>
-
-        </div>
-    </DragDropContext>
-    
+            );
+          })}
+        </DragDropContext>
+      </div>
     </div>
-  </>
+  );
 }
+
+export default App;
