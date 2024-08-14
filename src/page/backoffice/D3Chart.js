80622ff0e31c06a97c7134f30f0ca009f74d7589
DESKTOP-GFBT1N7\GunP <gundash1@gmail.com>
2024-08-14 10:28:04 +0700

    d3 refactor chart

--- src/page/backoffice/D3Chart.js
+++ src/page/backoffice/D3Chart.js
@@ -3,7 +3,34 @@ import { useEffect, useRef } from "react";
 
 export default function D3Chart(){
     const ref = useRef();
-
+    var data = [
+        {Country: "A", Value: .08167},
+        {Country: "B", Value: .01492},
+        {Country: "C", Value: .02780},
+        {Country: "D", Value: .04253},
+        {Country: "E", Value: .12702},
+        {Country: "F", Value: .02288},
+        {Country: "G", Value: .02022},
+        {Country: "H", Value: .06094},
+        {Country: "I", Value: .06973},
+        {Country: "J", Value: .00153},
+        {Country: "K", Value: .00747},
+        {Country: "L", Value: .04025},
+        {Country: "M", Value: .02517},
+        {Country: "N", Value: .06749},
+        {Country: "O", Value: .07507},
+        {Country: "P", Value: .01929},
+        {Country: "Q", Value: .00098},
+        {Country: "R", Value: .05987},
+        {Country: "S", Value: .06333},
+        {Country: "T", Value: .09056},
+        {Country: "U", Value: .02758},
+        {Country: "V", Value: .01037},
+        {Country: "W", Value: .02465},
+        {Country: "X", Value: .00150},
+        {Country: "Y", Value: .01971},
+        {Country: "Z", Value: .00074}
+      ];
     useEffect(() => {
         // set the dimensions and margins of the graph
         const margin = { top: 30, right: 30, bottom: 70, left: 60 },
@@ -18,11 +45,7 @@ export default function D3Chart(){
         .attr("height", height + margin.top + margin.bottom)
         .append("g")
         .attr("transform", `translate(${margin.left},${margin.top})`);
-
-        // Parse the Data
-        d3.csv(
-        "https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/7_OneCatOneNum_header.csv"
-        ).then(function (data) {
+        
         // X axis
         const x = d3
             .scaleBand()
@@ -38,7 +61,7 @@ export default function D3Chart(){
             .style("text-anchor", "end");
 
         // Add Y axis
-        const y = d3.scaleLinear().domain([0, 13000]).range([height, 0]);
+        const y = d3.scaleLinear().domain([0, 0.5]).range([height, 0]);
         svg.append("g").call(d3.axisLeft(y));
 
         // Bars
@@ -51,7 +74,7 @@ export default function D3Chart(){
             .attr("width", x.bandwidth())
             .attr("height", (d) => height - y(d.Value))
             .attr("fill", "#5f0f40");
-        });
+        
     }, []);
 
     return <div className="text-center my-auto">
