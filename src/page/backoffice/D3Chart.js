b9bc1f9af8394fce5ced59582f41e8be390d38b1
DESKTOP-GFBT1N7\GunP <gundash1@gmail.com>
2024-08-16 23:10:24 +0700

    chart with threshold

--- src/page/backoffice/D3Chart.js
+++ src/page/backoffice/D3Chart.js
@@ -5,44 +5,35 @@ export default function D3Chart(){
     let ref = useRef();
 
     var data = [
-        {Country: "A", Value: .08167},
-        {Country: "B", Value: .01492},
-        {Country: "C", Value: .02780},
-        {Country: "D", Value: .04253},
-        {Country: "E", Value: .12702},
-        {Country: "F", Value: .02288},
-        {Country: "G", Value: .02022},
-        {Country: "H", Value: .06094},
-        {Country: "I", Value: .06973},
-        {Country: "J", Value: .00153},
-        {Country: "K", Value: .00747},
-        {Country: "L", Value: .04025},
-        {Country: "M", Value: .02517},
-        {Country: "N", Value: .06749},
-        {Country: "O", Value: .07507},
-        {Country: "P", Value: .01929},
-        {Country: "Q", Value: .00098},
-        {Country: "R", Value: .05987},
-        {Country: "S", Value: .06333},
-        {Country: "T", Value: .09056},
-        {Country: "U", Value: .02758},
-        {Country: "V", Value: .01037},
-        {Country: "W", Value: .02465},
-        {Country: "X", Value: .00150},
-        {Country: "Y", Value: .01971},
-        {Country: "Z", Value: .00074}
+        {Country: "A", Value: .08167 * 100},
+        {Country: "B", Value: .01492 * 100},
+        {Country: "C", Value: .02780 * 100},
+        {Country: "D", Value: .04253 * 100},
+        {Country: "E", Value: .12702 * 100},
+        {Country: "F", Value: .02288 * 100},
+        {Country: "G", Value: .02022 * 100},
+        {Country: "H", Value: .06094 * 100},
+        {Country: "I", Value: .06973 * 100},
+        {Country: "J", Value: .00153 * 100},
+        {Country: "K", Value: .00747 * 100},
+      
       ];
 
     const data2 = data.filter(x=>x);
     const [tempD,setTempD] = useState(data)
     useEffect(() => { createChart(tempD) }, []);
-    
+
+    // const max = Math.max(...data.map(i=>i.Value)) +10;
+    const max = 20;
+    const svgX = 900;
+    const svgY = 600;
+
     const createChart = (dataInput) =>{
-        
+
         // set the dimensions and margins of the graph
-        const margin = { top: 30, right: 30, bottom: 70, left: 60 },
-        width = 500 - margin.left - margin.right,
-        height = 500 - margin.top - margin.bottom;
+        const margin = { top: 30, right: 60, bottom: 70, left: 60 },
+        width = svgX - margin.left - margin.right,
+        height = svgY - margin.top - margin.bottom;
 
         // append the svg object to the body of the page
         const svg = d3
@@ -58,7 +49,7 @@ export default function D3Chart(){
             .scaleBand()
             .range([0, width])
             .domain(dataInput.map((d) => d.Country))
-            .padding(0.2);
+            .padding(.6);
         svg
             .append("g")
             .attr("transform", `translate(0, ${height})`)
@@ -68,7 +59,7 @@ export default function D3Chart(){
             .style("text-anchor", "end");
 
         // Add Y axis
-        const y = d3.scaleLinear().domain([0, 0.5]).range([height, 0]);
+        const y = d3.scaleLinear().domain([0, max]).range([height, 0]);
         svg.append("g").call(d3.axisLeft(y));
 
         // Bars
@@ -78,22 +69,52 @@ export default function D3Chart(){
             .join("rect")
             .attr("x", (d) => x(d.Country))
             .attr("y", (d) => y(d.Value))
-            .attr("width", x.bandwidth())
+            .attr("width", 60)
             .attr("height", (d) => height - y(d.Value))
             .attr("fill", "#5f0f40");
+
+        //threshold
+        svg.append('line')
+            .style("stroke", "red")
+            .style("stroke-width", 2)
+            .attr("x1",0)
+            .attr("y1",y(max * (20/100)))
+            .attr("x2",width)
+            .attr("y2",y(max * (20/100)))
         
+        svg.append("text")
+            .attr("class", "myText")
+            .attr("x", width )
+            .attr("y", y(max * (20/100)))
+            .attr("dominant-baseline", "ideographic")
+            .text("Low20%" );
+
+        svg.append('line')
+            .style("stroke", "black")
+            .style("stroke-width", 2)
+            .attr("x1",0)
+            .attr("y1",y(max * (60/100)))
+            .attr("x2",width)
+            .attr("y2",y(max * (60/100)))
+        
+        svg.append("text")
+            .attr("class", "myText")
+            .attr("x", width )
+            .attr("y", y(max * (60/100)))
+            .attr("dominant-baseline", "ideographic")
+            .text("Warning " + max * (60/100));
     }
 
     const sortChart = () => {
         let sortD = tempD
         sortD.sort((u,v)=>u.Value-v.Value)
-        setTempD(sortD);
+        // setTempD(sortD);
         clearChart();
         createChart(sortD);
     }
 
     const resetChart = () => {
-        setTempD(data2);
+        // setTempD(data2);
         clearChart();
         createChart(data2);
     }
@@ -103,13 +124,14 @@ export default function D3Chart(){
         d.firstChild.remove();
     }
 
+  
     return <div >
         <div className="text-center my-auto">
             <button onClick={sortChart}>Sort</button>
             <button onClick={resetChart}>Reset</button>
         </div>
         <div className="text-center my-auto">
-            <svg width={500} height={500} id="barchart" ref={ref} />
+            <svg width={svgX} height={svgY} id="barchart" ref={ref} />
         </div>
     </div> 
 };
