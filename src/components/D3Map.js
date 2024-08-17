198fffa0bbe204f983ea5e115fbf3857e9ff5788
DESKTOP-GFBT1N7\GunP <gundash1@gmail.com>
2024-08-17 09:47:48 +0700

    page map v2

--- src/components/D3Map.js
+++ src/components/D3Map.js
@@ -1,29 +1,39 @@
 import * as d3 from "d3";
 import { useEffect, useRef, useState } from "react";
+import { feature } from "topojson";
 
 export default function D3Map(){
 
     // append the svg object to the body of the page
     const ref = useRef();
-    const width = 600;
-    const height = 600;
+    const svgX = 600;
+    const svgY = 600;
     const margin = {left:30, right:30, top:10, bottom: 10}
-    const dataGeo = d3.json('https://unpkg.com/world-atlas@2.0.2/countries-110m.json').then(data => {
-        // const countries = feature( data, data.objects.countries )
-        console.log(data)
-        // console.log(countries)
-    }
-    )
 
+    // const svg = d3.select('svg')
+    const projection = d3.geoMercator();
+    const geoPath = d3.geoPath().projection(projection);
     const svg = d3
         .select(ref.current)
         .append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
+        .attr("width", svgX + margin.left + margin.right)
+        .attr("height", svgY + margin.top + margin.bottom)
+
+    const dataGeo = d3.json('https://unpkg.com/world-atlas@2.0.2/countries-110m.json').then(data => {
+        const countries = feature( data, data.objects.countries )
+        // console.log(data)
+        // console.log(countries)
+        svg.selectAll('path').data(countries.features).enter().append('path').attr('d',geoPath)
+    }
+    )
+
+
     return <>
         <div className="text-center">
             12345
-            
+            <div className="text-center my-auto">
+                <svg width={svgX} height={svgY} id="map" ref={ref} />
+            </div>
         </div>
     </>
 }
