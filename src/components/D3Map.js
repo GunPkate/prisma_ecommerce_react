470c4008dc11b4bdfbd649d688d06d9048097f06
DESKTOP-GFBT1N7\GunP <gundash1@gmail.com>
2024-08-18 12:16:44 +0700

    test map by jp geo

--- src/components/D3Map.js
+++ src/components/D3Map.js
@@ -1,8 +1,8 @@
 import * as d3 from "d3";
 import { useEffect, useRef, useState } from "react";
 import { feature } from "topojson";
-import thaiGeo from "../componentMap/thai.geo.json"
-// https://stackblitz.com/edit/d3map?file=index.ts
+import thaiGeo from "../componentMap/jp.geo.json"
+
 export default function D3Map(){
 
     // append the svg object to the body of the page
@@ -10,23 +10,36 @@ export default function D3Map(){
     const svgX = 600;
     const svgY = 600;
     const margin = {left:30, right:30, top:10, bottom: 10}
+    const centerPos = [137.0, 38.2];
+    const scale = 2000;
 
-    // const svg = d3.select('svg')
-    const projection = d3.geoMercator();
-    const geoPath = d3.geoPath().projection(projection);
+    const projection = d3.geoMercator()
+    .center(centerPos)
+    .translate([svgX / 2, svgY / 2])
+    .scale(scale);
+    const path = d3.geoPath().projection(projection);
 
     const createMap = () =>{
         const svg = d3
             .select(ref.current)
-            .append("svg")
-            .attr("width", svgX + margin.left + margin.right)
-            .attr("height", svgY + margin.top + margin.bottom)
 
-        const dataGeo = d3.json('https://unpkg.com/world-atlas@2.0.2/countries-110m.json').then(data => {
-            const countries = feature( data, data.objects.countries )
-            // console.log(data)
-            // console.log(countries)
-            svg.selectAll('path').data(countries.features).enter().append('path').attr('d',geoPath)
+            
+            svg
+            .select(`#map-container`)
+            .append(`svg`)
+            .attr(`viewBox`, `0 0 ${svgX} ${svgY}`)
+            .attr(`width`, `100%`)
+            .attr(`height`, `100%`);
+
+   
+        svg.selectAll('path').data(thaiGeo.features).enter().append('path').attr('d',path).attr(`stroke`, `#666`)
+        .attr(`stroke-width`, 0.25)
+        .attr(`fill`, `#2566CC`)
+        .attr(`fill-opacity`, (item) => {
+
+            console.log(item)
+
+          return Math.random();
         })
     }
 
@@ -36,8 +49,9 @@ export default function D3Map(){
         <div className="text-center">
             12345
             <div className="text-center my-auto">
-                <svg width={svgX} height={svgY} id="map" ref={ref} />
+                <svg width={svgX} height={svgY} id="map-container" ref={ref} />
             </div>
         </div>
+
     </>
 }
