4f6e07cb7f780f06b6ee809874aeba54b166f1b5
DESKTOP-GFBT1N7\GunP <gundash1@gmail.com>
2024-09-01 16:23:13 +0700

    set sale ranking and change opacity

--- src/components/D3Map.js
+++ src/components/D3Map.js
@@ -6,13 +6,26 @@ export default function D3Map(prop){
 
     // append the svg object to the body of the page
     const ref = useRef();
-    const svgX = 600;
-    const svgY = 600;
+    const svgX = 700;
+    const svgY = 700;
     const margin = {left:30, right:30, top:10, bottom: 10}
+    let sum = 0
     //JP cordinate
     // const centerPos = [137.0, 38.2];
+    const data = [
+        {   id: 1, name: "TT", totalOrder: 3, totalValue: 58000, location: 3,},
+        {   id: 2, name: "TT", totalOrder: 3, totalValue: 20000, location: 46,},
+        {   id: 3, name: "GP", totalOrder: 3, totalValue: 1000, location: 4 },
+        {   id: 4, name: "TT", totalOrder: 3, totalValue: 8000, location: 5,},
 
-    const createMap = ( centerPosInput, scaleInput) =>{
+    ]
+
+    const setOpacityMap = () => {
+        data.forEach(x=> sum += x.totalValue);
+        console.log(sum);
+    }
+
+    const createMap = ( centerPosInput, scaleInput, maxOpacity) =>{
             //Thai coordinate
 
         let projection = d3.geoMercator()
@@ -36,62 +49,61 @@ export default function D3Map(prop){
         svg.selectAll('path').data(thaiGeo.features).enter().append('path').attr('d',path).attr(`stroke`, `#666`)
         .attr(`stroke-width`, 0.25)
         .attr(`fill`, (item)=>{
-            if(!item.properties.name.includes('Bangkok')){
-                return `#2566CC`
-            }else{
-                return `#DC143C`
+            for(let i =0; i < data.length; i++){
+                if(item.geometry.index === data[i].location){
+                    return `#DC143C`
+                }
             }
+            return `#2566CC` 
         })
         .attr(`fill-opacity`, (item) => {
             // console.log(item)
-            if(!item.properties.name.includes('Bangkok')){
-                return Math.random();
-            }else{
-                return .8
+            for(let i =0; i < data.length; i++){
+                if(item.geometry.index === data[i].location){
+                    let opac = data[i].totalValue/maxOpacity/10;
+                    opac += .6 +opac*4
+                    console.log("opac", opac)
+                    return  opac;   
+                }
             }
+            return .8;
         })
         .attr(`id`, (item) => { return item.properties.name })
-        .on('click', item => {
+        .attr(`class`, (item) => { return item.geometry.index })
+        .on('click', (item) => {
             let text = document.getElementById("info").innerHTML 
             setLabel(item.target.id)
-            
-            if(item.target.id === text){ 
-                setLabel("Click") 
-            }
-            
-            let targetColor = document.getElementById(`${item.target.id}`)
-            // if(item.target.id !== text){
-            //     targetColor.setAttribute("fill", "#000");
-            //     targetColor.setAttribute("fill-opacity", "1");
-            // }else{
-            //     targetColor.setAttribute("fill", "#000");
-            //     targetColor.setAttribute("fill-opacity", "1");
-            // }
- 
+            setSaleID(item.target.getAttribute('class'))
         });
     }
 
-    useEffect(() => { createMap([100.0, 12.2],2000) },[])
+    useEffect(() => {
+        let sum = 0; 
+        data.forEach(x => sum += x.totalValue);
+        console.log("sum",sum);
+        setsaleSum(sum)
+        createMap([100.0, 12.2],2000, sum) 
+    },[])
     
     const [label,setLabel] = useState("Click");
-    // const zoom = () => {
-    //     removeMap()
-    //     createMap([100.0, 12.2],4000) 
-    // }
+    const [sale,setSale] = useState([ {   id: 0, name: "", totalOrder: 0, totalValue: 0, location: 0 }]);
+    const [saleId,setSaleID] = useState( 0 );
+    const [saleSum,setsaleSum] = useState( 0 );
 
-    // const moveMap = (di) => {
-    //     const moveNum = 20 
-    //     if(di === 'L'){
-    //         // centerPos[1] += 20
-    //         // console.log(centerPos)
-    //     } 
-    //     createMap();
-    // }
-
-    // const removeMap = () => {
-    //     let d = document.getElementById("map-container")
-    //     console.log(d)
-    // }
+   const setSaleUI = () =>{
+        console.log(saleId)
+        const temp = {   id: 0, name: "", totalOrder: 0, totalValue: 0, location: 0 }
+        for (let i = 0; i < data.length; i++) {
+            if(saleId == data[i].location){
+                temp.id = data[i].id
+                temp.location = data[i].location
+                temp.totalOrder = data[i].totalOrder
+                temp.totalValue = data[i].totalValue
+                // console.log(data[i])
+            }  
+        }
+        setSale([temp])
+    }
 
     return <>
         <div className="text-center">
@@ -103,13 +115,60 @@ export default function D3Map(prop){
             <div >
                 
                 <div className="row text-center my-auto">
-                    <div className="col-2">
+                    <div className="col-3">
+
+                        <table className="mb-3">
+                            <tr>
+                                <th className="col-1">Rank</th>
+                                <th className="col-7">Location</th>
+                                <th className="col-4">Order</th>
+                            </tr>
+                            {data.map(x=>{
+                                return <tr>
+                                     <td>  {x.id}</td>
+                                    {/* <div>  {x.name}</div> */}
+                                    <td colSpan={3}>  {x.location}</td>
+                                    <td>  {x.totalOrder}</td>
+                                    {/* <div> Sales {x.totalValue} </div> */}
+                                    {/* <div> Sales { Number.parseFloat( x.totalValue/saleSum*100 ).toFixed(2)} % </div> */}
+                                </tr>
+                            })}
+                        </table>
+
                         <div className="mx-2 px-2 py-2 bg-info" id="info">{label}</div>
+                        <div className="mt-4 ">
+                            {sale.map(x=> {
+                                if(x.location !== 0){
+                                    return <div className="mx-2 px-2 mt-4  bg-info" style={{ opacity: 1}}>
+                                        <div>
+                                            <div> Rank {x.id}</div>
+                                            {/* <div>  {x.name}</div> */}
+                                            <div> Order {x.totalOrder}</div>
+                                            <div> Sales {x.totalValue} </div>
+                                            <div> Sales { Number.parseFloat( x.totalValue/saleSum*100 ).toFixed(2)} % </div>
+                                            <div> LocationId {x.location}</div>
+                                        </div>
+                                    </div>
+                                }else{
+                                    return <div className="mx-2 px-2 mt-4  bg-info" style={{ opacity: 0.01}}>
+                                    <div >
+                                        <div> Rank </div>
+                                        {/* <div>  {x.name}</div> */}
+                                        <div> Order </div>
+                                        <div> Sales  </div>
+                                        <div> Sales  % </div>
+                                        <div> LocationId </div>
+                                    </div>
+                                </div>
+                                }
+                            } )}
+                        </div>
+        
                         </div>
                         <div className="col-8">
-                            <svg width={svgX} height={svgY} id="map-container" ref={ref} />
+                            <svg width={svgX} height={svgY} id="map-container" onClick={()=>{setSaleUI()}} ref={ref} />
                         </div>
-                    <div className="col-2"></div>
+                    <div className="col-1"></div>
                 </div>
             </div>
         </div>
